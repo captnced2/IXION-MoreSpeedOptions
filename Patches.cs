52273fcc176bc0d35e9e495f061e5006a07dbebb
captnced <cedric.elsaesser1603@gmail.com>
2025-01-13 16:21:04 +0100

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using BulwarkStudios.Stanford.Common.UI;
+using BulwarkStudios.Stanford.Core.GameStates;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MoreSpeedOptions;
+
+public class Patches
+{
+    [HarmonyPatch(typeof(Game), nameof(Game.SetState))]
+    public static class GameStatePatcher
+    {
+        public static void Postfix(Game.STATE state)
+        {
+            if (state == Game.STATE.PLAYING)
+                if (!GameObject.Find(ButtonManager.buttons.First().name))
+                {
+                    ButtonManager.setupDefaultButtons();
+                    foreach (var b in ButtonManager.buttons) b.create();
+                    ButtonManager.fixCyclesSize();
+                }
+        }
+    }
+
+    [HarmonyPatch(typeof(UICycleClockIndicator), nameof(UICycleClockIndicator.RefreshCycleText))]
+    public static class CycleTextPatcher
+    {
+        public static void Postfix(UICycleClockIndicator __instance)
+        {
+            var zeros = "";
+            for (var i = __instance.txtCycleValue.m_characterCount;
+                 i < __instance.txtCycleValue.m_characterCount + ButtonManager.buttonsCount * 2;
+                 i++) zeros += '0';
+            __instance.txtCycleValue.text = "<color=#444444>" + zeros + "</color>" + __instance.txtCycleValue.text;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using BulwarkStudios.GameSystems.Ui;
+using BulwarkStudios.Stanford.Common.UI;
+using BulwarkStudios.Stanford.Core.TimeManagement;
+using BulwarkStudios.Utils.UI;
+using Cpp2IL.Core.Extensions;
+using HarmonyLib;
+using IMHelper;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace MoreSpeedOptions;
+
+[BepInPlugin(Guid, Name, Version)]
+[BepInProcess("IXION.exe")]
+[BepInDependency("captnced.IMHelper")]
+public class Plugin : BasePlugin
+{
+    private const string Guid = "captnced.MoreSpeedOptions";
+    private const string Name = "MoreSpeedOptions";
+    private const string Version = "1.0.0";
+    internal new static ManualLogSource Log;
+    internal static ConfigFile config;
+
+    public override void Load()
+    {
+        Log = base.Log;
+        config = Config;
+        ButtonManager.addButton("Super Faster", "Hotkey for changing the game speed to two times the max", 8, KeyCode.Alpha4);
+        ButtonManager.addButton("Super Fastest", "Hotkey for changing the game speed to four times the max", 16, KeyCode.Alpha5);
+        var harmony = new Harmony(Guid);
+        harmony.PatchAll();
+        foreach (var patch in harmony.GetPatchedMethods())
+            Log.LogInfo("Patched " + patch.DeclaringType + ":" + patch.Name);
+        Log.LogInfo("Loaded \"" + Name + "\" version " + Version + "!");
+    }
+}
+
+public class Button
+{
+    private readonly int speed;
+
+    public Button(string Name, string description, int Speed, KeyCode DefaultKey)
+    {
+        name = Name;
+        speed = Speed;
+        key = DefaultKey;
+        _ = new SettingsHelper.KeySetting(ButtonManager.buttonSection, Name, description, key, trigger, true);
+        ButtonManager.buttonsCount++;
+    }
+
+    public KeyCode key { get; set; }
+    public string name { get; }
+
+    public void create()
+    {
+        var superFastButton = GameObject.Find("SuperFast");
+        var newButton = Object.Instantiate(superFastButton, superFastButton.transform.parent);
+        newButton.name = name;
+        newButton.transform.SetAsLastSibling();
+        newButton.GetComponent<UiButtonTriggerUnityEvent>().enabled = false;
+        newButton.GetComponent<UITooltipHoverHelper>().enabled = false;
+        Action buttonClicked = delegate { trigger(); };
+        newButton.GetComponent<UiButton>().add_OnTriggered(buttonClicked);
+        newButton.GetComponent<UiButton>().overrideState = UiButton.STATE.CUSTOM1;
+        Action<TimeManager.TIME_SCALES> timeScaleChanged = delegate { ButtonManager.timeChanged(name); };
+        TimeManager.add_OnTimeChanged(timeScaleChanged);
+        loadButtonTexture();
+    }
+
+    private void loadButtonTexture()
+    {
+        var button = GameObject.Find(name);
+        var stream =
+            typeof(Plugin).Assembly.GetManifestResourceStream("MoreSpeedOptions.assets." + name + ".png");
+        if (stream == null) return;
+        var oldSprite = button.transform.FindChild("Icon").GetComponent<Image>().sprite;
+        var texture = new Texture2D(2, 2);
+        texture.LoadImage(stream.ReadBytes());
+        var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+            oldSprite.textureRectOffset);
+        button.transform.FindChild("Icon").GetComponent<Image>().sprite = sprite;
+    }
+
+    public void trigger()
+    {
+        TimeManager.SetTime(TimeManager.TIME_SCALES.X4);
+        TimeManager.TimeScale = speed;
+        ButtonManager.deselectAllOtherButtons(name);
+        var uiButton = GameObject.Find(name).GetComponent<UiButton>();
+        uiButton.overridedState = true;
+        uiButton.UpdateState(UiButton.STATE.CUSTOM1);
+    }
+}
+
+public class ButtonManager
+{
+    public static int buttonsCount;
+    public static List<Button> buttons = new();
+    public static SettingsHelper.SettingsSection buttonSection = new("MoreSpeedOptions");
+    public static UIPauseStripes pause;
+
+    public static void addButton(string name, string description, int speed, KeyCode defaultKey)
+    {
+        buttons.Add(new Button(name, description, speed, defaultKey));
+    }
+
+    public static void fixCyclesSize()
+    {
+        var cyclesTxt = GameObject.Find("CyclesTxt");
+        cyclesTxt.GetComponent<LayoutElement>().preferredWidth = 113 + 29 * buttonsCount;
+    }
+
+    public static void setupDefaultButtons()
+    {
+        foreach (var uiButton in GameObject.Find("SuperFast").transform.parent.GetComponentsInChildren<UiButton>())
+        {
+            Action buttonSelected = delegate { deselectAllOtherButtons(uiButton.gameObject.name); };
+            uiButton.add_OnTriggered(buttonSelected);
+        }
+
+        pause = GameObject.Find("Canvas/Pause").GetComponent<UIPauseStripes>();
+    }
+
+    public static void timeChanged(string buttonName)
+    {
+        var uiButton = GameObject.Find(buttonName).GetComponent<UiButton>();
+        uiButton.overridedState = false;
+        uiButton.UpdateState(UiButton.STATE.NORMAL);
+    }
+
+
+    public static void deselectAllOtherButtons(string except)
+    {
+        foreach (var uiButton in GameObject.Find("SuperFast").transform.parent.GetComponentsInChildren<UiButton>())
+            if (!(uiButton.gameObject.name == except))
+            {
+                uiButton.overridedState = false;
+                uiButton.UpdateState(UiButton.STATE.NORMAL);
+            }
+    }
+}
